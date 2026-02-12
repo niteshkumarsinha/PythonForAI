91d421e304f32e1402999245b2758ea88816f7ef
Nitesh Kumar <nitesh111sinha@gmail.com>
2026-02-12 21:05:52 +0530

    sets

---
+++ 1-Python-Basics/sets.py
@@ -0,0 +1,19 @@
+empty_set = set()
+print(empty_set)  # Output: set()
+
+
+numbers = {1, 2, 3, 4, 5}
+print(numbers)  # Output: {1, 2, 3, 4,
+
+fruits = {"apple", "banana", "cherry"}
+print(fruits)  # Output: {'apple', 'banana', 'cherry'}
+print(type(fruits))  # Output: <class 'set'>
+
+
+print(set([1, 2, 3, 4, 5]))  # Output: {1, 2, 3, 4, 5}
+print(set("hello"))  # Output: {'h', 'e', 'l', 'o'}
+
+scores = [85, 90, 75, 80, 85]
+unique_scores = set(scores)
+print(unique_scores)  # Output: {75, 80, 85, 90}
+
